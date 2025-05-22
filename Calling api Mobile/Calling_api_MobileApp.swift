37acaa71fd7e7a5ec51156525d7f448a49ad2f96
Samante <samante@bsd220.org>
2025-05-21 22:28:44 -0500

    Initial Commit

---
+++ Calling api Mobile/Calling_api_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  Calling_api_MobileApp.swift
+//  Calling api Mobile
+//
+//  Created by Samuel Amante on 5/21/25.
+//
+
+import SwiftUI
+
+@main
+struct Calling_api_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Calling api MobileTests/Calling_api_MobileTests.swift
@@ -0,0 +1,17 @@
+//
+//  Calling_api_MobileTests.swift
+//  Calling api MobileTests
+//
+//  Created by Samuel Amante on 5/21/25.
+//
+
+import Testing
+@testable import Calling_api_Mobile
+
+struct Calling_api_MobileTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
